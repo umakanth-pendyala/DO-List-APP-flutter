b75ae271fc3246495ec2626fb270418d8c36241c
= <=>
2020-07-09 00:59:32 +0530

    fire base integrated into the app for storage

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -1,7 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/widgets.dart';
 import 'package:today_plans/models/tasks.dart';
 
 class TaskData extends ChangeNotifier {
+  dynamic userTasks;
+
+  TaskData() {
+    userTasks = Firestore.instance.collection('tasks');
+    getDocuments();
+  }
+
+  getDocuments() async {
+    _tasks = [];
+    QuerySnapshot snapshot = await userTasks.getDocuments();
+    snapshot.documents.forEach((element) {
+      print(element.data['taskName']);
+      _tasks.add(Task(nameOfTheTask: element.data['taskName']));
+    });
+    notifyListeners();
+  }
+
   List<Task> _tasks = [
     Task(nameOfTheTask: 'Complete the Tasks app'),
     Task(nameOfTheTask: 'Eat something'),
@@ -12,20 +30,43 @@ class TaskData extends ChangeNotifier {
     return _tasks.length;
   }
 
-  void addTask(String taskName) {
+  void addTask(String taskName) async {
     _tasks.add(Task(nameOfTheTask: taskName));
+    await userTasks.add({'taskName': taskName, 'taskCompleted': false});
     notifyListeners();
   }
 
   Task getSpecificTask(int index) => _tasks[index];
 
-  void updateTask(Task task) {
+  void updateTask(Task task) async {
     task.updateTaskStatus();
+    final QuerySnapshot snapshot = await userTasks
+        .limit(1)
+        .where('taskName', isEqualTo: task.nameOfTheTask)
+        .getDocuments();
+    DocumentSnapshot doc = snapshot.documents[0];
+    if (doc.exists) {
+      doc.reference.updateData(
+        {
+          'taskName': task.nameOfTheTask,
+          'taskCompleted': task.taskStatus,
+        },
+      );
+    }
     notifyListeners();
   }
 
-  void deleteSpecificTask(int index) {
+  void deleteSpecificTask(int index) async {
+    String taskName = _tasks[index].nameOfTheTask;
     _tasks.removeAt(index);
+    final QuerySnapshot snapshot = await userTasks
+        .limit(1)
+        .where('taskName', isEqualTo: taskName)
+        .getDocuments();
+    DocumentSnapshot doc = snapshot.documents[0];
+    if (doc.exists) {
+      doc.reference.delete();
+    }
     notifyListeners();
   }
 }
